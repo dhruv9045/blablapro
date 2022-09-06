8316543c81cba7811e989f8a6aa60d965c2e5b6a
dhruv9045 <singh1999dhruv@gmail.com>
2022-09-06 23:11:53 +0530

    Initialize-06092022

---
+++ android/app/src/main/kotlin/com/dksdev/blablapro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dksdev.blablapro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:blablapro/view/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blabla Pro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
